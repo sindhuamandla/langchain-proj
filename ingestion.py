b7e15a568fcec4c8850babe308c427c384a0e8a1
Sindhu A <sindhuamandla@gmail.com>
2026-04-28 19:48:23 +0530

    Add retrieval

--- ingestion.py
+++ ingestion.py
@@ -2,15 +2,17 @@ import os
 from dotenv import load_dotenv
 
 from langchain_community.document_loaders import TextLoader
-from langchain_text_splitters import CharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 #from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain_ollama import OllamaEmbeddings
 from langchain_pinecone import PineconeVectorStore
 
 load_dotenv()
 
 if __name__ == '__main__':
     print("Ingesting...")
-    print(os.environ['PINECONE_API_KEY'])
+    #print(os.environ['PINECONE_API_KEY'])
 
     loader = TextLoader(
     "C:/Users/rajir/Desktop/langchain-course/mediumblog1.txt",
@@ -19,10 +21,21 @@ if __name__ == '__main__':
     document = loader.load()
 
     print("splitting...")
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=100
+)
     texts = text_splitter.split_documents(document)
     print(f"created {len(texts)} chunks")
 
     #embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
+    embeddings = OllamaEmbeddings(model="nomic-embed-text")
+    vectorstore = PineconeVectorStore.from_documents(
+        texts,
+        embedding=embeddings,
+        index_name=os.environ["INDEX_NAME"]
+    )
+
+    print("finish")
 
     
